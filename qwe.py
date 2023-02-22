b290f46dabac426197aa6dc220c0883337d727f0
shikimxri <125442832+shikimxri@users.noreply.github.com>
2023-02-22 12:02:41 +0300

    Add files via upload

---
+++ qwe.py
@@ -0,0 +1,21 @@
+n1 = input("Введите число\n")
+
+#1
+a = (str(n1))
+print(a , type (a))
+
+#2
+b = (float(n1))
+print(b , type (b))
+
+#3
+c = (complex(n1))
+print(c , type (c))
+
+#4
+d = (bool(n1))
+print(d , type (d))
+
+#5
+e = (int(float(n1)))
+print(e , type (e))
